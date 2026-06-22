8c7d9db0002cd8a005828ca33a61821816bbf31b
Emre <eozsahin1993@gmail.com>
2026-06-22 14:39:29 -0400

    feat(backend): add db structs and interfaces

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"log"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"log"
 )
 
 func main() {
---
+++ backend/pkg/db/store.go
@@ -0,0 +1,31 @@
+package db
+
+import "time"
+import "context"
+
+type ChunkMetadata struct {
+	ExternalId string `json:"external_id"`
+	FileUri    string `json:"file_uri"`
+	SourceName string `json:"source_name"`
+	MimeType   string `json:"mime_type"`
+	SourceType string `json:"source_type"`
+	RawJson    string `json:"raw_json"`
+}
+
+type ChunkDbRecord struct {
+	ID         string          `json:"id"`
+	Chunk      []byte          `json:"chunk"`
+	ChunkHash  string          `json:"chunk_hash"`
+	ChunkIndex int             `json:"chunk_index"`
+	Vector     []float32       `json:"vector"`
+	CreatedAt  time.Time       `json:"created_at"`
+	UpdatedAt  time.Time       `json:"updated_at"`
+	Metadata   ChunkDbMetadata `json:"metadata"`
+}
+
+type VectorDb interface {
+	connect(ctx context.Context, connectionUrl string) error
+	createTable(ctx context.Context, name string)
+	insertRecord(ctx context.Context, tableName string, record ChunkDbRecord)
+	querySimilarVectors(ctx context.Context, tableName string, vector []float32, topK int) []ChunkDbRecord
+}
